bcea3b8ca3302346dfd4ce38d2ed0a026efdb389
Ronnie Pavoni <ronniepavoni@gmail.com>
2022-08-31 17:04:23 +0300

    Add restAPI requests server-client

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { createRequire } from "module";
+import fetch from "node-fetch";
 const require = createRequire(import.meta.url);
 
 const { v4: uuidv4 } = require("uuid");
@@ -13,17 +14,16 @@ const fs = require("fs");
 const path = require("path");
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import getMAC, { isMAC } from "getmac";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 import WebSocket, { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8080, clientTracking: true });
-const userData = {
-    id: {},
-    ipv4: {},
-    clients: {},
-};
+
+let clients = 0;
+const userData = [clients];
 
 // EXPRESS!
 // Static files
@@ -34,21 +34,27 @@ app.get("/data", (req, res) => {
 app.get("/", (req, res) => {
     res.sendFile(`index.html`);
 });
-
 app.listen(3000, function () {
     console.log(`Server running at http://localhost:3000`);
 });
 
 // WEBSOCKET!
-wss.on("connection", (ws, req) => {
-    userData.ipv4 = req.socket.remoteAddress.slice(7);
-    userData.id = uuidv4();
-    connections[userData.id] = ws;
-    userData.clients = wss.clients.size;
-    console.log(
-        `Client ${userData.id} - ${userData.ipv4} connected! ${userData.clients}`
-    );
 
+wss.on("connection", (ws, req) => {
+    ws.on("message", (event) => {
+        const user = {
+            id: "",
+            ipv4: "",
+            mac: "",
+        };
+        user.mac = event.toString();
+        console.log(user.mac);
+        user.ipv4 = req.socket.remoteAddress.slice(7);
+        user.id = uuidv4();
+        userData[0] = wss.clients.size;
+        userData.push(user);
+        console.log(userData);
+    });
     ws.on("close", function disconnect() {
         userData.clients -= 1;
         console.log(
@@ -56,3 +62,45 @@ wss.on("connection", (ws, req) => {
         );
     });
 });
+
+const objTest = {
+    id: "something",
+    ip: "34",
+    action: "wakeup",
+};
+const action = objTest.action;
+let postFetchTime = 5000;
+switch (action) {
+    case "shutdown":
+        setTimeout(() => {
+            try {
+                fetch("http://192.168.0.228:4000/shutdown", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }).then((response) => {
+                    console.log("sos!");
+                });
+            } catch (e) {
+                console.log(e);
+            }
+        }, postFetchTime);
+        break;
+    case "wakeup":
+        setTimeout(() => {
+            try {
+                fetch("http://192.168.0.228:4000/wakeup", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }).then((response) => {
+                    console.log("sos!");
+                });
+            } catch (e) {
+                console.log(e);
+            }
+        }, postFetchTime);
+        break;
+}
--- node.js
+++ node.js
@@ -1,21 +1,28 @@
-// import WebSocket, { WebSocketServer } from "ws";
-
-// const socket = new WebSocket("ws://localhost:3000");
-// socket.addEventListener("open", function (event) {
-//     socket.send("Hello Server!");
-// });
-
-// socket.addEventListener("message", function (event) {
-//     console.log("Message from server ", event.data);
-// });
-
-// socket.addEventListener("close", function (event) {
-//     console.log("The connection has been closed");
-// });
-
+import { createRequire } from "module";
 import WebSocket from "ws";
+import getMAC, { isMAC } from "getmac";
+const require = createRequire(import.meta.url);
+const express = require("express");
+const app = express();
+const wol = require("wol");
+const mac = getMAC();
+// console.log(mac);
 const ws = new WebSocket("ws://192.168.0.167:8080");
-
 ws.on("open", function open() {
-    console.log("connected!");
+    console.log("Connected!");
+    ws.send(mac);
 });
+
+app.post("/shutdown", function (req, res) {
+    console.log("shutting down!");
+    require("child_process").exec("shutdown -s", console.log);
+});
+
+app.post("/wakeup", function (req, res) {
+    console.log("wakeup!!");
+    // wol.wake(mac, function (err, res) {
+    //     console.log(res);
+    // });
+});
+
+app.listen(4000);
